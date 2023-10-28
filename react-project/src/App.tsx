d87651c990a84e53a53df07701b6d35467eb9d2b
Fault1err <oalkozlova@gmail.com>
2023-10-29 01:09:07 +0300

    feat: create initial markup

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <>
-      <div>Hello World!</div>
+      <div className="App">
+        <div className="top-section">
+          <input type="text" placeholder="Enter word" />
+          <button>Search</button>
+        </div>
+        <div className="bottom-section">
+          <h2>Search Results</h2>
+          <ul className="results-block">
+            <li>Result 1</li>
+            <li>Result 2</li>
+            <li>Result 3</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
