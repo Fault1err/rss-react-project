2e7cb3419405e6974be77f730cfbfb78dd1bbdfa
Fault1err <oalkozlova@gmail.com>
2023-11-05 02:27:12 +0300

    feat: add disabling pagination btns functional

--- react-project/src/components/Pagination.tsx
+++ react-project/src/components/Pagination.tsx
@@ -11,19 +11,38 @@ const Pagination: FunctionComponent<PaginationProps> = ({
   currentPage,
   onPageChange,
 }) => {
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === totalPages;
+
   return (
     <div className="pagination">
-      <button className="pag_btn" onClick={() => onPageChange(1)}>
+      <button
+        className="pag_btn"
+        onClick={() => onPageChange(1)}
+        disabled={isFirstPage}
+      >
         {'<<'}
       </button>
-      <button className="pag_btn" onClick={() => onPageChange(currentPage - 1)}>
+      <button
+        className="pag_btn"
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={isFirstPage}
+      >
         {'<'}
       </button>
       <span className="pag_span">{currentPage}</span>
-      <button className="pag_btn" onClick={() => onPageChange(currentPage + 1)}>
+      <button
+        className="pag_btn"
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={isLastPage}
+      >
         {'>'}
       </button>
-      <button className="pag_btn" onClick={() => onPageChange(totalPages)}>
+      <button
+        className="pag_btn"
+        onClick={() => onPageChange(totalPages)}
+        disabled={isLastPage}
+      >
         {'>>'}
       </button>
     </div>
