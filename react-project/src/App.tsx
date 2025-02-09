772f64ce18997168b53b1f52005486801cefb163
f4ult1err <f4ult1err@gmail.com>
2025-02-09 23:44:38 +0300

    feat: add 404 page

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
 import CharPage from './pages/CharacterPage';
+import NotFoundPage from './pages/NotFoundPage'; 
 
 function App(): React.JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<MainPage />}>
         <Route path="/char/:id" element={<CharPage />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* 404 Route */}
       </Route>
     </Routes>
   );
